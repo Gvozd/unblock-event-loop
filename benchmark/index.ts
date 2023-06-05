d66bfb2c0a7f0eb8a23d81079cde25a9e13e3408
Gvozd <gvozdev.viktor@gmail.com>
2023-06-06 01:34:44 +0300

    try p-map - not worked

--- benchmark/index.ts
+++ benchmark/index.ts
@@ -4,12 +4,15 @@ import { createServer, get } from 'node:http';
 import process from 'node:process';
 import { setTimeout as delay } from 'node:timers/promises';
 import assert from 'node:assert';
+import pMap from 'p-map';
 import { unblock } from '../src/index.ts';
 
 const countOfServers = 3;
 const port = 8000;
-const requestTime = 0;
+const requestTime = 50;
 const url = `http://127.0.0.1:${port}/`;
+const countOfTasks = 100e3;
+const step = 5000;
 
 if (cluster.isPrimary) {
   console.log(`Primary ${process.pid} is running`);
@@ -20,9 +23,9 @@ if (cluster.isPrimary) {
   await delay(1e3);
   console.log('base check', await request(url));
 
-  for (let count = 2000; count < 40e3; count += 2000) {
-    await test('simple', count, simpleRequest);
-    await test('unblocked', count, unblockedRequest);
+  for (let concurrency = step; concurrency < 40e3; concurrency += step) {
+    await test('simple', countOfTasks, concurrency, simpleRequest);
+    await test('unblocked', countOfTasks, concurrency, unblockedRequest);
     await delay(100);
   }
 
@@ -37,32 +40,32 @@ if (cluster.isPrimary) {
   console.log(`Worker ${process.pid} started`);
 }
 
-async function test(name, count, executor) {
+async function test(name: string, count: number, concurrency: number, executor: (url: string) => Promise<void>) {
+  const tasks = Array(count).fill(url);
   const start = performance.now();
-  const tasks = Array(count).fill(url).map(executor);
   try {
-    await Promise.all(tasks);
+    await pMap(tasks, executor, {concurrency});
   } catch(e) {
     await delay(100);
   }
   console.log(`${name}:${count}: ${Math.round(performance.now() - start) / 1000}s`);
 }
 
-async function simpleRequest(testUrl) {
+async function simpleRequest(testUrl: string) {
   const result = await request(testUrl);
   assert(result === 'Hello Node.js Server');
 }
 
-async function unblockedRequest(testUrl) {
+async function unblockedRequest(testUrl: string) {
   await unblock();
   const result = await request(testUrl);
   assert(result === 'Hello Node.js Server');
 }
 
-async function request(requestUrl) {
+async function request(requestUrl: string) {
   return new Promise((resolve, reject) => {
     const req = get(requestUrl, (resp) => {
-      const data = [];
+      const data: string[] = [];
       resp.on('data', (chunk) => {
         data.push(chunk);
       });
